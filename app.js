34473f2b315c9a129b5aa4271081e28d554b28e5
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-07-31 03:10:26 +0530

    app changes

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 require ("./config/database").connect
-const express = require ()
+const express = require ("express")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+
+// import model 
+
+const app = express()
+app.use(express.json())
+
+// routes 
+app.get("/", (req, res )=>{
+    res.send("Hello home routes")
+
+})
