12daf384f64597c61aa266e0a2a97706a2667fda
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-07-31 16:51:52 +0530

    try catch update

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-require ("./config/database").connect
+require ("./config/database").connect()
 const express = require ("express")
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcryptjs")
 
 // import model 
+const User = require("./models/user")
+const { check } = require("app")
 
 const app = express()
 app.use(express.json())
@@ -11,5 +13,20 @@ app.use(express.json())
 // routes 
 app.get("/", (req, res )=>{
     res.send("Hello home routes")
+})
+app.post("/register",(req, res )=>{
+    try{
+        // collection all information from the front end 
+        const {firstnam, lastname, email, token}= req.body
+        // validate the data if exists 
+        if (!(email && password && lastname && fistname))
+        res.status(401).send
+    }
 
+    // check if the user exist in the database 
+    catch(error){
+        console.log(error);
+        console.log("error of the respond route")
+
+}
 })
